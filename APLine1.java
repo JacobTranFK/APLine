e3c5ffcd56ff125aef32933019306d05a6a58556
JacobTranFK <101596854+JacobTranFK@users.noreply.github.com>
2022-12-15 11:29:12 -0800

    Create APLine1.java
    Makes the line

---
+++ APLine1.java
@@ -0,0 +1,20 @@
+public class APLine {
+  int a;
+  int b;
+  int c;
+  public APLine(int a, int b, int c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  public double getSlope() {
+    return -1.0*a/b;
+  }
+  public boolean isOnLine(int x, int y) {
+    if((a*x)+(b*y)+c==0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
